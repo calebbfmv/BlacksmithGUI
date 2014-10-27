2bb9eef3cae314eb24d1e7396ce179e6cdd42dfe
calebbfmv <tda96@me.com>
2014-10-26 23:05:09 -0500

    Woops

--- src/main/java/me/calebbfmv/blacksmithgui/ability/abilities/HealthAbility.java
+++ src/main/java/me/calebbfmv/blacksmithgui/ability/abilities/HealthAbility.java
@@ -2,8 +2,6 @@ package me.calebbfmv.blacksmithgui.ability.abilities;
 
 import me.calebbfmv.blacksmithgui.ability.Ability;
 import me.calebbfmv.blacksmithgui.ability.AbilityType;
-import org.apache.commons.lang.StringUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -29,6 +27,5 @@ public class HealthAbility extends Ability {
         PotionEffect effect = new PotionEffect(PotionEffectType.HEALTH_BOOST, 10000000, level * 2);
         player.addPotionEffect(effect);
         player.setHealth(player.getMaxHealth());
-        player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + StringUtils.capitalize(type.name()) + ChatColor.GRAY + "] " + ChatColor.RED + "is now activated.");
     }
 }
