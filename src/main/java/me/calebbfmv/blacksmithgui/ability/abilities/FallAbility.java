01a58892a27c7471641f2c3f0a1ef7162762a602
calebbfmv <tda96@me.com>
2014-10-26 23:05:02 -0500

    More Fixes
    Trying to get DO tShirt

--- src/main/java/me/calebbfmv/blacksmithgui/ability/abilities/FallAbility.java
+++ src/main/java/me/calebbfmv/blacksmithgui/ability/abilities/FallAbility.java
@@ -3,7 +3,6 @@ package me.calebbfmv.blacksmithgui.ability.abilities;
 import me.calebbfmv.blacksmithgui.BlacksmithGUI;
 import me.calebbfmv.blacksmithgui.ability.Ability;
 import me.calebbfmv.blacksmithgui.ability.AbilityType;
-import org.apache.commons.lang.StringUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
@@ -22,6 +21,5 @@ public class FallAbility extends Ability {
         int level = getLevel(player);
         int percentage = 10 * level;
         player.setMetadata("fall", new FixedMetadataValue(BlacksmithGUI.getInstance(), percentage));
-        player.sendMessage(ChatColor.GRAY + "[" + ChatColor.GOLD + StringUtils.capitalize(type.name()) + ChatColor.GRAY + "] " + ChatColor.RED + "is now activated.");
     }
 }
